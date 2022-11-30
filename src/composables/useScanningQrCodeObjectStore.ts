85be254cc84d65bb5feb1453044649ac0ffd37b6
Thatchakon Jom-ud <dsjinj@gmail.com>
2022-11-30 12:00:07 +0900

    feat(scanning history): added delete

--- src/composables/useScanningQrCodeObjectStore.ts
+++ src/composables/useScanningQrCodeObjectStore.ts
@@ -4,30 +4,67 @@ import { IQrCodeHistory } from './useQrCode'
 export interface IUseScanningQrCodeObjectStore {
   writeScanningQrCodeItem: (qrCodeItem: IQrCodeHistory) => void
   getAllScanningQrCode: () => Promise<Array<IQrCodeHistory>>
+  deleteScanningQrCodeItem: (id: number) => Promise<undefined>
 }
 
 export default function useScanningQrCodeObjectStore (db: Ref<IDBDatabase | null>): IUseScanningQrCodeObjectStore {
-  const writeScanningQrCodeItem = (qrCodeItem: IQrCodeHistory) => {
-    const scanningQrCodeObjectStore = db.value?.transaction('qrcode_histories', 'readwrite').objectStore('qrcode_histories')
-    scanningQrCodeObjectStore?.add(
-      qrCodeItem
-    )
+  const writeScanningQrCodeItem = (qrCodeItem: IQrCodeHistory): Promise<IDBValidKey> => {
+    return new Promise((resolve, reject) => {
+      const scanningQrCodeObjectStore = db.value?.transaction('qrcode_histories', 'readwrite').objectStore('qrcode_histories')
+      const scanningItem = scanningQrCodeObjectStore?.add(
+        qrCodeItem
+      )
+      if (scanningItem) {
+        scanningItem.onsuccess = (ev: any) => {
+          resolve(ev.target.result)
+        }
+        scanningItem.onerror = (ev: any) => {
+          reject(new Error(`Write failed: ${ev.target.errorCode}`))
+        }
+      } else {
+        reject(new Error('DB is not ready.'))
+      }
+    })
+  }
+  const deleteScanningQrCodeItem = (id: number): Promise<undefined> => {
+    return new Promise((resolve, reject) => {
+      const scanningQrCodeObjectStore = db.value?.transaction('qrcode_histories', 'readwrite').objectStore('qrcode_histories')
+      const deletingItem = scanningQrCodeObjectStore?.delete(id)
+      if (deletingItem) {
+        deletingItem.onsuccess = (ev: any) => {
+          resolve(ev.target.result)
+        }
+        deletingItem.onerror = (ev: any) => {
+          reject(new Error(`Delete failed: ${ev.target.errorCode}`))
+        }
+      } else {
+        reject(new Error('DB is not ready.'))
+      }
+    })
   }
   const getAllScanningQrCode = (): Promise<Array<IQrCodeHistory>> => {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       const qrcodeHistoryList: Array<IQrCodeHistory> = []
       const scanningQrCodeObjectStore = db.value?.transaction('qrcode_histories', 'readwrite').objectStore('qrcode_histories')
-      // eslint-disable-next-line
-      scanningQrCodeObjectStore!.getAll().onsuccess = (ev: any) => {
-        ev.target.result.forEach((element: any) => {
-          qrcodeHistoryList.push(element)
-        })
-        resolve(qrcodeHistoryList)
+      const gettingAll = scanningQrCodeObjectStore?.getAll()
+      if (gettingAll) {
+        gettingAll.onsuccess = (ev: any) => {
+          ev.target.result.forEach((element: any) => {
+            qrcodeHistoryList.push(element)
+          })
+          resolve(qrcodeHistoryList)
+        }
+        gettingAll.onerror = (ev: any) => {
+          reject(new Error(`Get failed: ${ev.target.errorCode}`))
+        }
+      } else {
+        reject(new Error('DB is not ready.'))
       }
     })
   }
   return {
     writeScanningQrCodeItem,
-    getAllScanningQrCode
+    getAllScanningQrCode,
+    deleteScanningQrCodeItem
   }
 }
