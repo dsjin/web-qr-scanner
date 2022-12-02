b3272aa1ae5c402413015f75444fa1dde1fc8acd
Thatchakon Jom-ud <dsjinj@gmail.com>
2022-12-02 14:00:17 +0900

    feat(GeneratingHistoryDetailCard): added virtual list

--- src/components/qrcode/GeneratingHistoryDetailCard.vue
+++ src/components/qrcode/GeneratingHistoryDetailCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="info.show"
-    class="absolute top-0 left-0 right-0 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-screen overflow-y-scroll z-30"
+    class="absolute top-0 left-0 right-0 bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 h-screen overflow-hidden z-30"
   >
     <div
       class="absolute right-4 top-4 text-white font-bold text-md rounded-full h-10 w-10 bg-gray-900 flex items-center justify-center cursor-pointer z-40"
@@ -17,7 +17,10 @@
       >
         History
       </h1>
-      <div class="p-5 md:p-10 bg-gray-500 text-2xl text-white my-10 md:my-20 divide-y divide-dashed h-1/2">
+      <div
+        class="p-5 md:p-10 bg-gray-500 text-2xl text-white my-10 md:my-20"
+        :class="{'h-[calc(100vh-10rem)] md:h-[calc(100vh-16rem)]': !busy && displayingList.length > 0}"
+      >
         <template
           v-if="busy"
         >
@@ -61,91 +64,110 @@
           v-else
         >
           <template
-            v-if="displayingList.length > 0"
+            v-if="displayingList.length != 0"
           >
-            <div
-              v-for="(qrcode, index) in displayingList"
-              :key="`${qrcode.data}-${qrcode.timestamp}`"
-              class="flex flex-col p-4"
+            <dynamic-scroller
+              :items="displayingList"
+              :min-item-size="100"
+              class="h-full"
             >
-              <div
-                class="flex justify-center items-center w-full bg-white rounded-md shadow-xl"
-              >
-                <div
-                  v-html="qrcode.html.svg"
-                  class="lg:w-1/5 md:w-1/2 w-full"
-                />
-              </div>
-              <div
-                class="flex flex-col md:flex-row w-full my-4"
-              >
-                <p
-                  class="flex-grow flex-initial w-full md:w-3/5 text-left break-all"
-                >
-                  {{ qrcode.data }}
-                </p>
-                <p
-                  class="flex-grow flex-initial w-full md:w-2/5 md:text-center text-right"
-                >
-                  {{ utils.timestampToDateString(qrcode.timestamp) }}
-                </p>
-                <div
-                  class="flex justify-end mt-4 md:mt-0 flex-grow flex-initial w-full md:w-10"
+              <template v-slot="{ item: qrcode, index, active }">
+                <dynamic-scroller-item
+                  :item="qrcode"
+                  :active="active"
+                  :size-dependencies="[
+                    qrcode.data
+                  ]"
+                  :data-index="index"
                 >
                   <div
-                    class="text-white font-bold text-md rounded-full h-10 w-10 bg-gray-900 flex items-center justify-center cursor-pointer"
-                    @click="toggleShowAction(qrcode)"
+                    class="wrapper"
                   >
-                    <template
-                      v-if="!qrcode.action.show"
+                    <div
+                      class="flex flex-col p-4"
                     >
-                      ▼
-                    </template>
-                    <template
-                      v-else
-                    >
-                      ▲
-                    </template>
+                      <div
+                        class="flex justify-center items-center w-full bg-white rounded-md shadow-xl"
+                      >
+                        <div
+                          v-html="qrcode.html.svg"
+                          class="lg:w-1/5 md:w-1/2 w-full"
+                        />
+                      </div>
+                      <div
+                        class="flex flex-col md:flex-row w-full my-4"
+                      >
+                        <p
+                          class="flex-grow flex-initial w-full md:w-3/5 text-left break-all"
+                        >
+                          {{ qrcode.data }}
+                        </p>
+                        <p
+                          class="flex-grow flex-initial w-full md:w-2/5 md:text-center text-right"
+                        >
+                          {{ utils.timestampToDateString(qrcode.timestamp) }}
+                        </p>
+                        <div
+                          class="flex justify-end mt-4 md:mt-0 flex-grow flex-initial w-full md:w-10"
+                        >
+                          <div
+                            class="text-white font-bold text-md rounded-full h-10 w-10 bg-gray-900 flex items-center justify-center cursor-pointer"
+                            @click="toggleShowAction(qrcode)"
+                          >
+                            <template
+                              v-if="!qrcode.action.show"
+                            >
+                              ▼
+                            </template>
+                            <template
+                              v-else
+                            >
+                              ▲
+                            </template>
+                          </div>
+                        </div>
+                      </div>
+                      <div
+                        v-if="qrcode.action.show"
+                        class="flex flex-col w-full"
+                      >
+                        <div
+                          v-if="qrcode.action.isAbleToShareQrcode"
+                          class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
+                          @click="shareQrcode(index)"
+                        >
+                          <p
+                            class="truncate"
+                          >
+                            Share
+                          </p>
+                        </div>
+                        <div
+                          class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
+                          @click="saveQrcode(index)"
+                        >
+                          <p
+                            class="truncate"
+                          >
+                            Save an Image
+                          </p>
+                        </div>
+                        <div
+                          class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
+                          @click="deleteQrcode(index)"
+                        >
+                          <p
+                            class="truncate"
+                          >
+                            Delete
+                          </p>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-              <div
-                v-if="qrcode.action.show"
-                class="flex flex-col w-full"
-              >
-                <div
-                  v-if="qrcode.action.isAbleToShareQrcode"
-                  class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
-                  @click="shareQrcode(index)"
-                >
-                  <p
-                    class="truncate"
-                  >
-                    Share
-                  </p>
-                </div>
-                <div
-                  class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
-                  @click="saveQrcode(index)"
-                >
-                  <p
-                    class="truncate"
-                  >
-                    Save an Image
-                  </p>
-                </div>
-                <div
-                  class="text-white font-bold text-md rounded-full h-10 w-auto p-3 bg-gray-900 flex items-center justify-center cursor-pointer mb-4"
-                  @click="deleteQrcode(index)"
-                >
-                  <p
-                    class="truncate"
-                  >
-                    Delete
-                  </p>
-                </div>
-              </div>
-            </div>
+                </dynamic-scroller-item>
+              </template>
+            </dynamic-scroller>
           </template>
           <template
             v-else
